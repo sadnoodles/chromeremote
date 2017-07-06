be1591800d71192ccc385e60980288f960c4a60f
sadnoodles <sadnoodles@gmail.com>
2017-07-06 19:30:01 +0800

    level up!

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from distutils.core import setup
 setup(
     name="chromeremote",
-    version="0.1.0",
+    version="0.2.0",
     description="Chrome Remote Dev Tools",
     author="sadnoodles",
     author_email="sadnoodles@gmail.com",
     url="https://github.com/sadnoodles/chromeremote",
-    download_url="https://github.com/sadnoodles/chromeremote/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/sadnoodles/chromeremote/archive/v0.2.0.tar.gz",
     license="GPL",
     py_modules=["chromeremote"],
     install_requires=[
